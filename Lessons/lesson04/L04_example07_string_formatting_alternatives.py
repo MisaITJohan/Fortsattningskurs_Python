aac383e55bebb34b5f8e8218836bc276a94f1b20
MisaITJohan <johan.marmen@misa.se>
2024-09-18 16:01:54 +0200

    Apply changes from 2024-HT1-W04

--- Lessons/lesson04/L04_example07_string_formatting_alternatives.py
+++ Lessons/lesson04/L04_example07_string_formatting_alternatives.py
@@ -6,20 +6,21 @@ a_list = [1, 2, 3]
 
 # Variant 1, f-strängar
 print("f-strängar:")
-print(f"""Ett nummer: {a_number}\nLite text: {some_text}\nEn lista: {a_list}""")
+print(f"Ett nummer: {a_number}\nLite text: {some_text}\nEn lista: {a_list}")
 
 print()
 
 # Variant 2, format()-metoden:
 print("format()-metoden:")
-print("""Ett nummer: {}\nLite text: {}\nEn lista: {exempel}""".format(
-    a_number, some_text, exempel=a_list))
+print("Ett nummer: {}\nLite text: {}\nEn lista: {exempel}".format(
+    a_number, some_text, exempel=a_list
+    ))
 
 print()
 
 # Variant 3, (det äldsta sättet) %-mönstret:
 print("%-mönstret:")
-print("""Ett nummer: %d\nLite text: %s\nEn lista: %s""" % (a_number, some_text, a_list))
+print("Ett nummer: %d\nLite text: %s\nEn lista: %s" % (a_number, some_text, a_list))
 
 # Använd aldrig variant 3.
 
@@ -29,9 +30,10 @@ print()
 # Exempel på när man ska hämta från en dict:
 a_dict = {"name":"Johan", "age":34}
 
+# Med f-sträng:
+print(f"Hämtat från en dict: {a_dict['name']} är {a_dict["age"]} år gammal.")
+
+# Med format():
 print("Hämtat från en dict: {0[name]} är {0[age]} år gammal.".format(a_dict))
 # alternativt
 print("Hämtat från en dict: {name} är {age} år gammal.".format(**a_dict))
-
-# Jämfört med:
-print(f"Hämtat från en dict: {a_dict["name"]} är {a_dict["age"]} år gammal.")
