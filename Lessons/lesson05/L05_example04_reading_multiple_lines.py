f0d9979c27eacf8624251e2a05031ef7d8086a27
MisaITJohan <johan.marmen@misa.se>
2025-11-26 12:02:04 +0100

    Apply preparations for 2025-HT2-L05

--- Lessons/lesson05/L05_example04_reading_multiple_lines.py
+++ Lessons/lesson05/L05_example04_reading_multiple_lines.py
@@ -2,7 +2,7 @@
 
 with open("new_file.txt", "r", encoding="utf-8") as file:
     for row in file:
-        print(row.strip()) # Tar bort eventuella mellanslag och radbrytningar
+        print(row.strip())  # Tar bort eventuella mellanslag och radbrytningar
 
     # Nästa rad är bortkommenterad p.g.a. hur filer läses.
     #print(file.readlines())
