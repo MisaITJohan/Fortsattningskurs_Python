e7ccd140eaf2c68c799a4d84587b7d1ef2686c55
MisaITJohan <johan.marmen@misa.se>
2026-01-13 11:44:46 +0100

    Apply changes from 2025-HT2-W08

--- Lessons/lesson08/L08_example03_buttons_live.py
+++ Lessons/lesson08/L08_example03_buttons_live.py
@@ -7,11 +7,11 @@ root = tk.Tk()
 root.title("Knappexempel")
 root.geometry("400x300")
 
-# Vi skapar en variabel för att räkna klick på vår knapp
+# Vi skapar en variabel för att räkna klick på vår knapp.
 counter = 0
 
 def button_clicked():
-    """En funktion som ökar counter och ändrar text-egenskapen på result_label"""
+    """En funktion som ökar counter och ändrar text-egenskapen på result_label."""
     global counter  # Utan denna så skapas en ny counter-variabel i funktionen
     counter += 1
     result_label.config(
@@ -29,8 +29,10 @@ instruction_label.pack(pady=10)
 
 # Skapa en knapp och ställ in egenskapen command.
 # command ska peka på en funktion. Notera att jag inte lägger till några
-# parenteser då funktionen inte ska köras när knappen skapas.
+# parenteser efter button_clicked då funktionen inte ska köras när knappen
+# skapas.
 button = tk.Button(
+    root,
     text="Klicka på mig!",
     command=button_clicked,
     )
