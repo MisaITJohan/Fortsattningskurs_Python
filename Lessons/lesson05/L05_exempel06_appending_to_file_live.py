41c772cd0d461d0256d10d4451ec23de840eb54e
MisaITJohan <johan.marmen@misa.se>
2026-03-03 11:11:14 +0100

    Apply changes from 2026-VT1-L05

--- Lessons/lesson05/L05_exempel06_appending_to_file_live.py
+++ Lessons/lesson05/L05_exempel06_appending_to_file_live.py
@@ -4,5 +4,6 @@
 with open("new_file.txt", "a", encoding="utf-8") as file:
     file.write("Mer data som ska läggas till i slutet av filen.\n")
 
-# Notera att ovanstående inte lägger till något radbyte om vi inte lägger in
+
+# Notera att ovanstående inte lägger in något radbyte om vi inte lägger in
 # ett \n i slutet av strängen.
