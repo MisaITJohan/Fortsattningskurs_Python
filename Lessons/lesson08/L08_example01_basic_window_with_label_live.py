9852b4d72588e73e53817a82257027d5113b9bfb
MisaITJohan <johan.marmen@misa.se>
2026-03-25 11:25:40 +0100

    Apply changes from 2026-VT1-L08

--- Lessons/lesson08/L08_example01_basic_window_with_label_live.py
+++ Lessons/lesson08/L08_example01_basic_window_with_label_live.py
@@ -1,38 +1,40 @@
 # Exempel på hur man skapar ett simpelt fönster med hjälp av tkinter.
 
 # När man importerar tkinter så är det väldigt vanligt att man gör det med
-# namnet tk. Antagligen för att tydliggöra att det är Tk-verktygen man använder.
+#   namnet tk. Antagligen för att tydliggöra att det är Tk-verktygen man
+#   använder.
 import tkinter as tk
 
-# När man skapar huvudfönstret så använder man, per standard, namnet "root".
+#När man skapar huvudfönstret så använder man, per standard, namnet "root".
 root = tk.Tk()
 
 
-# Med hjälp av metoder på fönstret så kan vi ändra på egenskaper hos vårt
-# fönster.
+# Med hjälp av metoder på fönstret (Tk-objektet) så kan vi ändra på egenskaper
+#   hos vårt fönster.
+#   .title() ändrar texten som står i överkant av fönstret.
 root.title("Ett simpelt fönster")
 
 
-# Med hjälp av .geometry så kan vi styra hur stort fönstret ska vara.
-# Storleken skickas, förvirrande nog, som en sträng. I dess simplaste form är
-# strängen "BREDDxHÖJD".
+# Med hjälp av .geometry() så kan vi styra hur stort fönstret ska vara.
+# Storleken skickas, förvirrande nog, so men sträng. I dess simplaste form är
+#   strängen "BREDDxHÖJD".
 # Just för att vi ska ha det där x:et i mitten måste vi använda en sträng.
 root.geometry("400x300")
 
 
 # Om vi vill visa lite text så passar en Label väldigt bra.
-# När vi skapar en ny Widget, utöver tk.TK(), så måste vi skicka med vilken
-# tidigare Widget som den nya ska tillhöra. Det görs som det första
-# positionsargumentet.
+# När vi skapar en ny Widget, utöver tk.Tk(), så måste vi skicka med vilken
+#   tidigare Widget som den nya ska tillhöra. Det görs som det första
+#   positionsargumentet.
 # Därefter kan man skicka med flera nyckelordsargument för att konfigurera,
-# alltså ställa in, önskade värden för den nya Widgeten.
+#   alltså ställa in, önskade värden för den nya Widgeten.
 label = tk.Label(root, text="Etikett med text!")
 
-# För att vår Label ska synas måste vi använda en av de tre layouthanterarna
-# för att placera den i vårt fönster. Vi tittar på hur det fungerar i nästa
-# exempel.
+# För att vår Label ska synas måste vi använda en av de tre Layouthanterarna
+#   för att placera den i vårt fönster. Vi tittar på hur det fungerar i nästa
+#   exempel.
 label.pack(pady=20)
 
 # .mainloop() sätter igång Tk-instansen och gör att programmet fortsätter att
-# svara på input.
+#   svara på input.
 root.mainloop()
--- Lessons/lesson08/L08_example03_buttons_live.py
+++ Lessons/lesson08/L08_example03_buttons_live.py
@@ -26,11 +26,10 @@ instruction_label = tk.Label(
     )
 instruction_label.pack(pady=10)
 
-
 # Skapa en knapp och ställ in egenskapen command.
 # command ska peka på en funktion. Notera att jag inte lägger till några
-# parenteser efter button_clicked då funktionen inte ska köras när knappen
-# skapas.
+#   parenteser efter button_clicked då funktionen inte ska köras när
+#   knappen skapas.
 button = tk.Button(
     root,
     text="Klicka på mig!",
